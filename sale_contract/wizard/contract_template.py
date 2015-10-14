882d79b40c214416fcd78476fd39ea199d7920bd
HaojunZou <apollo_zhj@msn.com>
2015-10-14 15:08:25 +0200

    changed view in contract template form

--- sale_contract/wizard/contract_template.py
+++ sale_contract/wizard/contract_template.py
@@ -9,27 +9,25 @@ _logger = logging.getLogger(__name__)
 class sale_contract_wizard(models.TransientModel):
     _name = 'sale.contract.wizard'
 
-    template_id = fields.Many2one(comodel_name='account.analytic.account', string='Template of Contract')
+    template_id = fields.Many2one(comodel_name='account.analytic.account', string='Template of Contract', domain=(['type', '=', 'template']))
 
     @api.one
     def create_contract(self):
-        categ_wtime = self.env.ref('product.uom_categ_wtime') # Working time-type
-        service_uom = self.env['product.uom'].search([('category_id', '=', categ_wtime.id)]) # All uom of working time type
-        service_products = self.env['product.product'].search([('type', '=', 'service'),('uom_id','in',service_uom)])
-        other_products = self.env['product.product'].search([]) - service_products
-        service = self.env['product.product'].search([('type', '=', 'service'),('','','')])
-        goods = self.env['product.product'].search([('type', '=', 'consu')])
-        _logger.warning('<<<<<<<<<<< SERVICE >>>>>>>>>>>: %s' % service)
-        _logger.warning('<<<<<<<<<<< GOODS >>>>>>>>>>>: %s' % goods)
+        categ_wtime = self.env.ref('product.uom_categ_wtime')  # Working time-type
+        service_uom = [c.id for c in self.env['product.uom'].search([('category_id', '=', categ_wtime.id)])]  # All uom of working time type
+        service_products = [p.id for p in self.env['product.product'].search([('type', '=', 'service'), ('uom_id', 'in', service_uom)])]
+        other_products = [p.id for p in self.env['product.product'].search([]) if p.id not in service_products]
 
         for order in self.env['sale.order'].browse(self._context.get('active_ids')):
             # copy a record from a template to a contract
             contract = self.template_id.copy({
+                'partner_id': order.partner_id.id,
+                'manager_id': self._uid,
                 'date_start': fields.date.today(),
-                'amount_max': sum([l.price_subtotal for l in order.order_line if not l.product_id.id in [p.id for p in other_products]]),  # TODO: find out the product ids of goods
-                'hours_qtt_est': sum([l.price_subtotal for l in order.order_line if l.product_id.id in [p.id for p in service_products]]),  # TODO: find out the product ids of service
+                'amount_max': sum([l.price_subtotal for l in order.order_line if l.product_id.id in other_products]),
+                'hours_qtt_est': sum([l.price_subtotal for l in order.order_line if l.product_id.id in service_products]),
                 'invoice_on_timesheets': True,
-                'type': 'template',
+                'type': 'contract',
             })
             if self.template_id.date_start and self.template_id.date:
                 from_dt = datetime.strptime(self.template_id.date_start, tools.DEFAULT_SERVER_DATE_FORMAT)
