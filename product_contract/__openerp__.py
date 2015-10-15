a34863d3be6481a5907d46c9a3d0eca21d1da903
Robin Chatfield <robin.chatfield@gmail.com>
2015-10-15 17:19:32 +0200

    Added quantity and first invoice date calculations.

--- product_contract/__openerp__.py
+++ product_contract/__openerp__.py
@@ -6,16 +6,16 @@
 ##############################################################################
 
 {
-    'name': 'Prenumerations',
+    'name': 'Subscriptions',
     'version': '1.0',
     'category': 'Sale',
     'description': """
-Sell prenumerations.
+Tie a contract template to a product. A new contract will be generated upon sale. Contract templates have recurring invoices activated, enabling the sale of subscriptions.
 =========================================================================================================
     """,
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
-    'depends': ['account_analytic_analysis', 'website_sale'],
+    'depends': ['account_analytic_analysis', 'sale'],
     'data': [
         'product_contract_view.xml',
         ],
