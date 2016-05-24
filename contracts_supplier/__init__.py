b0c58d1d1ff4f1f397012005040e06812bc28bb7
anderswallenquist <anders.wallenquist@vertel.se>
2016-05-24 13:57:10 +0200

    contracts_supplier: new module

---
+++ contracts_supplier/__init__.py
@@ -0,0 +1,1 @@
+import contracts_supplier
---
+++ contracts_supplier/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2012 OpenERP s.a. (<http://openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Contracts',
+    'version': '0.1',
+    'category': 'Sales',
+    'description': """
+Adds contracts for suppliers.
+===================================================
+""",
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['account_analytic_analysis'],
+    'data': ['contracts_supplier_view.xml',
+    ],
+
+    'installable': True,
+    
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
