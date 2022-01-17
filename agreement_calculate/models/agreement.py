d3086fb42d3a910c3825337f028419b155e830a3
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-01-17 12:43:16 +0000

    Added uom for size

--- agreement_calculate/models/agreement.py
+++ agreement_calculate/models/agreement.py
@@ -39,22 +39,22 @@ class AgreementReport(models.Model):
             store=True,
             )
 
-    @api.depends("property_id", "property_id.area", "yearly_cost")
+    @api.depends("property_id", "property_id.size", "property_id.size_uom", "yearly_cost")
     def _yearly_cost_per_square_meter(self):
         for record in self:
             calculated_value = None
             try:
-                calculated_value = record.yearly_cost / record.property_id.area
+                calculated_value = record.yearly_cost / (float(record.property_id.size) * record.property_id.size_uom.factor_inv)
             except (TypeError, ZeroDivisionError) as e:
                 pass
             record.yearly_cost_per_square_meter = calculated_value
 
-    @api.depends("property_id", "property_id.employees", "property_id.area")
+    @api.depends("property_id", "property_id.employees", "property_id.size", "property_id.size_uom")
     def _square_meter_per_employee(self):
         for record in self:
             calculated_value = None
             try:
-                calculated_value = record.property_id.area / record.property_id.employees
+                calculated_value = float(record.property_id.size) * record.property_id.size_uom.factor_inv / record.property_id.employees
             except (TypeError, ZeroDivisionError) as e:
                 pass
             record.square_meter_per_employee = calculated_value
@@ -69,12 +69,12 @@ class AgreementReport(models.Model):
                 pass
             record.yearly_cost_per_employee = calculated_value
 
-    @api.depends("property_id", "property_id.area", "property_id.workplaces")
+    @api.depends("property_id", "property_id.size", "property_id.workplaces", "property_id.size_uom")
     def _square_meter_per_workplace(self):
         for record in self:
             calculated_value = None
             try:
-                calculated_value = record.property_id.area / record.property_id.workplaces
+                calculated_value = float(record.property_id.size) * record.property_id.size_uom.factor_inv / record.property_id.workplaces
             except (TypeError, ZeroDivisionError) as e:
                 pass
             record.square_meter_per_workplace = calculated_value
