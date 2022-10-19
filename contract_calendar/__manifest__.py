681c2e769a5d17fd9eadf5276521a66cf7a1aba8
Anders <anders.wallenquist@vertel.se>
2022-10-19 23:20:12 +0000

    contract_calendar: new module

---
+++ contract_calendar/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2021 Tecnativa - Jairo Llopis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Contract - Calendar",
+    "summary": "Adds calendar to a contract",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Marketing",
+    "website": "https://vertel.se",
+    "author": "Vertel AB",
+    "maintainers": ["Vertelab"],
+    "license": "AGPL-3",
+    "application": False,
+    "auto-install": False,
+    "installable": True,
+    "depends": [
+        "contract",
+        "calendar",
+    ],
+    "data": [
+        "views/contract.xml",
+    ],
+}
