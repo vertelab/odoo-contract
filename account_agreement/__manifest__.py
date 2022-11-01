fd18c0d413f458f5e08504dd1534e3276f7574a9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-01 16:39:49 +0100

    Update __manifest__.py

--- account_agreement/__manifest__.py
+++ account_agreement/__manifest__.py
@@ -1,16 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Copyright 2021 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Account - Extension to be used together with Agreement",
-    "summary": "TODO: Inherit variables from contract into agreement",
-    "version": "14.0.1.0.0",
-    "development_status": "Beta",
-    "category": "Marketing",
-    "website": "https://vertel.se",
-    "author": "Vertel AB",
-    "maintainers": ["Vertelab"],
-    "license": "AGPL-3",
+    'name': 'Contract: Account Agreement',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Account - Extension to be used together with Agreement.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales',
+    'description': """
+    TODO: Inherit variables from contract into agreement.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-contract/account_aggreement',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-contract',
+    # Any module necessary for this one to work correctly
     "application": False,
     "auto-install": True,
     "installable": True,
@@ -26,3 +57,4 @@
         "views/account_move.xml",
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
