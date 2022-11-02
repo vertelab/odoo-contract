1bb7628e5d9cb13a1b2021aeda9386dff5bfa15b
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-02 14:29:26 +0100

    Update __manifest__.py

--- website_contract_terms/__manifest__.py
+++ website_contract_terms/__manifest__.py
@@ -1,26 +1,50 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   www.vertel.se
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Website Contract Terms',
-    'version': '1.0',
-    'category': 'Sale',
+    'name': 'Contract: Website Contract Terms',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Terms on contracts',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales',
     'description': """
-Terms on contracts
-==================
+    Terms on contracts
+    ==================
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-contract/website_contract_terms',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'website': 'http://www.vertel.se',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-contract',
+    # Any module necessary for this one to work correctly
     'depends': ['website', 'account_analytic_analysis'],
     'data': [
         'website_contract_terms.xml',
         ],
     'installable': True,
 }
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
