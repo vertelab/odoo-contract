fcbdc14444f17765e65df8440421fdb83464fed0
Dmitri Iselund <dmitri.iselund@vertel.se>
2022-11-04 15:49:05 +0000

    contract_attendee: fixed date bug where you had to supply both end and start for the calendar tab in contract.contract contract_calendar: added compute method for the field "start"

--- contract_attendee/models/calendar_attendee.py
+++ contract_attendee/models/calendar_attendee.py
@@ -1,7 +1,7 @@
 from distutils.util import Mixin2to3
 import logging
 from odoo import models, fields, api, _
-from datetime import datetime
+import datetime
 
 _logger = logging.getLogger(__name__)
 
@@ -13,30 +13,27 @@ class ExtendAttendee(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         attendees = super().create(vals_list)
+        _logger.warning(vals_list)
         for attendee in attendees:
-            # _logger.warning(vals_list)
             partner = self.env['res.partner'].browse(vals_list[0]['partner_id'])
-            # _logger.warning(partner)
             user = partner.user_ids[0]
-            # _logger.warning(user)
+
             leave_periods = self.env['hr.leave'].search_read([('user_id', '=', user.id)],[])
-            # _logger.warning(leave_periods)
-            # _logger.warning(user)
-            # employee = user.employee_id[0]
-            # _logger.warning(employee)
-            # off_date_start = employee.leave_date_from
-            # off_date_end = employee.leave_date_to
+
             event = self.env['calendar.event'].browse(vals_list[0]['event_id'])
+            # _logger.warning(f"{event.id} {event.name} {event.start} {event.start_date}")
 
-            # _logger.warning(event)
-            # _logger.warning(f"{off_date_start} {off_date_end}")
-            # _logger.warning(f"{event_date_start} {event_date_end}")
-
-            # _logger.warning(leave_periods)
             for leave in leave_periods:
-                # _logger.warning(f"{leave['request_date_from']}     {leave['request_date_to']}")
-                # _logger.warning(f"{event.start_date}     {event.stop_date}")
-                if leave['request_date_from'] <= event.stop_date and event.start_date <= leave['request_date_to']:
-                    attendee.state = "tentative"
+                try:
+                    if leave['request_date_from'] <= event.stop_date and event.start_date <= leave['request_date_to']:
+                        attendee.state = "tentative"
+                except TypeError:
+                    if leave['request_date_from'] <= (event.start + datetime.timedelta(hours=event.duration)).date() and event.start.date() <= leave['request_date_to']:
+                        attendee.state = "tentative"
 
         return attendees    
+
+    def write(self, vals):
+        _logger.warning(vals)
+        res = super().write(vals)
+        return res
--- contract_calendar/models/contract.py
+++ contract_calendar/models/contract.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 
 from odoo import models, fields, api, _
+from datetime import datetime
 
 
 _logger = logging.getLogger(__name__)
@@ -12,8 +13,14 @@ class Contract(models.Model):
     _inherits = {'calendar.event': 'event_id'}
     event_id = fields.Many2one(comodel_name='calendar.event',
                     string='Calendar', auto_join=True, index=True, 
-                    ondelete="cascade", required=True)              
-                    
+                    ondelete="cascade", required=True)         
+
+    start = fields.Datetime(compute='_inherit_date', readonly=False)     
+
+    @api.depends("date_start")           
+    def _inherit_date(self):
+        self.start = self.date_start
+
     @api.model_create_multi
     def create(self, vals_list):
         contracts = self.env["contract.contract"]
@@ -21,10 +28,11 @@ class Contract(models.Model):
             event = self.env['calendar.event'].create({
                 'name': vals.get('name',),
                 'start_date': vals.get('date_start', fields.Date.today()),
-                'stop_date': vals.get('date_end', fields.Date.today()), 
+                'stop_date': vals.get('date_end', 'date_start'), 
                 'duration': vals.get('duration', 1), 
             })
             vals["event_id"] = event.id
+            _logger.warning(f"contract.contract BAPIDI {vals}")
             contract = super(Contract, self.with_context()).create(vals)
             event.contract_id = contract.id
             contracts += contract
