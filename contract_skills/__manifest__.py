aa53b10f0711585d7d30d6640f240878c2f7ed55
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-01-24 15:44:10 +0100

    Update __manifest__.py

--- contract_skills/__manifest__.py
+++ contract_skills/__manifest__.py
@@ -21,7 +21,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Contract: Contract Skills',
+    'name': 'Contract: Skills',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a tag containing skills',
