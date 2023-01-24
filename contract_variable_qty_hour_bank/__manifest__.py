aed87bc7ec954a9aca70e4156b4d49cbdff68a30
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-01-24 16:05:23 +0100

    Update __manifest__.py
    fine tunig the manifest

--- contract_variable_qty_hour_bank/__manifest__.py
+++ contract_variable_qty_hour_bank/__manifest__.py
@@ -1,13 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2023- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 #BORROWED MODULE FROM OCA
+
 {
-    "name": "Contract Variable Qty Hour Bank",
-    "summary": "Invoice hours exceeding an hour bank ",
-    "version": "14.0.0.0.0",
-    "category": "Contract Management",
-    "website": "https://vertel.se/apps/contract_variable_qty_hour_bank",
-    "author": "Vertel AB",
-    "license": "AGPL-3",
+    'name': 'Contract: Variable Qty Hour Bank',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Invoice hours exceeding an hour bank.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales',
+    'description': """
+    Adds timesheet to contract.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-contract/contract_variable_qty_hour_bank',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-contract',
+    # Any module necessary for this one to work correctly
     "installable": True,
     "depends": ["contract_variable_quantity", "hr_timesheet",'product','product_contract'],
     "data": [
@@ -16,3 +50,4 @@
             "views/contract_view.xml",
             ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
