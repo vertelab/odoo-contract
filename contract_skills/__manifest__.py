dcd73348a7d4c69261828d673cf6d7c2f9949a23
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-21 10:51:42 +0100

    Update __manifest__.py

--- contract_skills/__manifest__.py
+++ contract_skills/__manifest__.py
@@ -28,7 +28,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Contract',
+    'category': 'Sales',
     #'sequence': '1',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-contract/contract_skills',
