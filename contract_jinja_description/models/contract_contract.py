f752cd4d74725f5307e954212b0af3ad780249d7
Jakob Krabbe <jakob.krabbe@vertel.se>
2023-09-15 13:59:15 +0200

    contract_jinja_description: njinja fine tuning

--- contract_jinja_description/models/contract_contract.py
+++ contract_jinja_description/models/contract_contract.py
@@ -18,15 +18,41 @@ class ContractContract(models.Model):
     Overrides function in contract.contract, 
     so that we can use jinja when you create an invoice from a contract
     """
-    
-    
+
     def _prepare_recurring_invoices_values(self, date_ref=False):
         _logger.warning('_prepare_recurring_invoices_values running... date_ref = %s' % date_ref)
         invoices_values = super()._prepare_recurring_invoices_values(date_ref)
+        for inv in invoices_values:
+            inv['narration'] = self.note
+            _logger.warning(repr(inv['narration']))
+            inv['narration'] = self._render_template_jinja(
+
+                inv['narration'],
+                "contract.contract", 
+                [self.id]
+            )
+            tp=str(inv['narration']).split("'",1)
+            tp=tp[1][:-2]
+            tp=tp.replace('\\n', '\n')
+            inv['narration']=tp
+            _logger.warning(repr(inv['narration']))
+                
+                
+            n = 0
+            for line in inv['invoice_line_ids']:
+                line[2]['name'] = self._render_template_jinja(
+
+                    line[2]['name'],
+                    "contract.line", 
+                    [self.contract_line_ids[n].id]
+                )
+                tp=str(line[2]['name']).split("'",1)
+                tp=tp[1][:-2]
+                line[2]['name']=tp
+                n = n+1
+                # ~ _logger.warning(line[2]['name'])
 
         invoices_values_json=""
-        
-        
         
         try:
 
@@ -37,8 +63,6 @@ class ContractContract(models.Model):
             
             raise UserError('At least one of the values in the form can not be parsed with '\
                             'json.stringify check "website_quote_jinja_description" for more')
-        
-
 
         """
         In this function call we are giving it a string, a module and a list of ids. The thing that needs commenting is the 
@@ -48,19 +72,46 @@ class ContractContract(models.Model):
         want to reach through Jinja in this situation. That is the reason why we are doing this, because now, most of the time 
         you need to write alot less to reach the data that you need.
         """
+        
+        for line_id in self.contract_line_fixed_ids.ids:
+            new_invoices_values_json = self._render_template_jinja(
 
-        invoices_values_json = self._render_template_jinja(
+                invoices_values_json,
+                "contract.line", 
+                [line_id]
+                )
+            _logger.warning(f"{new_invoices_values_json=}")
+            # ~ _logger.warning(f"{new_invoices_values_json[line_id][0]=}")
+            # ~ _logger.warning(f"{new_invoices_values_json[line_id][1][2]=}")
+            d1_eval = eval(new_invoices_values_json[line_id].replace('false','False').replace('true','True'))
+            # ~ d1_eval = eval(new_invoices_values_json[line_id])
+            # ~ _logger.warning(f"{d1_eval[0]['invoice_line_ids']=}")
+            # ~ _logger.warning(f"{d1_eval[0]['invoice_line_ids'][n][2]['contract_line_id']=}")
+            for n in self.contract_line_fixed_ids.ids:
+                _logger.warning(n)
+                _logger.warning(n)
+                _logger.warning(n)
+                _logger.warning(n)
+                # ~ _logger.warning(f"{d1_eval[0]['invoice_line_ids'][n][2]['contract_line_id']=}")
+                _logger.warning(type(line_id))
+                _logger.warning(line_id)
+                # ~ if line_id = d1_eval[0]['invoice_line_ids'][n][2]['contract_line_id']:
+                    # ~ invoices_values = d1_eval[0]['invoice_line_ids'][n][2]['contract_line_id'] + invoices_values
+                    
+                
+            _logger.warning(f"{d1_eval=}")
+            _logger.warning(f"{invoices_values=}")
+            # ~ invoices_values[0]['invoice_line_ids'][n][2].update(d1_eval[0]['invoice_line_ids'][n][2])
+            _logger.warning(f"{invoices_values=}")
+            
+            # ~ _logger.warning(f"{new_invoices_values_json['invoice_filter_type_domain']}")
+            # ~ _logger.warning(f"In for, with line_id {line_id} result:{new_invoices_values_json['invoice_line_ids']=}")
+        # ~ invoices_note_json = self._render_template_jinja(
 
-            invoices_values_json,
-            "contract.line", 
-            self.contract_line_fixed_ids.ids
-            )
-        invoices_note_json = self._render_template_jinja(
-
-            invoices_values_json,
-            "contract.contract", 
-            [self.id]
-            )
+            # ~ invoices_values_json,
+            # ~ "contract.contract", 
+            # ~ [self.id]
+            # ~ )
         
         """
         Jinja believes that we have given it uniques strings for each of the ids connected to the module, but we are using this function
@@ -83,8 +134,8 @@ class ContractContract(models.Model):
         
 
         _logger.warning(f"{invoices_values=}")
-        _logger.warning(f"{invoices_note_json=}")
-        invoices_values[0]['narration']=invoices_note_json['note']
+        # ~ _logger.warning(f"{invoices_note_json=}")
+        # ~ invoices_values[0]['narration']=invoices_note_json['note']
         return invoices_values
     
 class DateEncoder(json.JSONEncoder):
