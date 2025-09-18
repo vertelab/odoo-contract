b8059ae20fc87185d3a72ec2c3519d58d0bed427
Marcus K <marcus.karlsson@vertel.se>
2025-09-18 11:02:05 +0000

    agreement_contract,pricelist_cpi: Updated code for index calculations

--- agreement_property/models/agreement.py
+++ agreement_property/models/agreement.py
@@ -6,6 +6,28 @@ from odoo import models, fields, api, _
 _logger = logging.getLogger(__name__)
 
 
+class Property(models.Model):
+    _description = "Agreement Property"
+    _inherit = 'property.property'
+    
+    #agreement_ids = fields.One2many('agreement', inverse='property_id')
+    
+    def action_agreement(self):
+        type_id = self.env.ref('agreement_legal.agreement_type_contract').id
+        return {
+            'name': _("Agreements"),
+            'type': 'ir.actions.act_window',
+            'res_model':'agreement',
+            'view_mode': 'kanban,list,form',
+            'domain': [('property_id', '=', self.id)],
+            'context': {
+                'default_property_id': self.id,
+                'default_name':self.name,
+                'default_agreement_type_id':type_id,
+            }
+        }
+
+
 class AgreementProperty(models.Model):
     _description = "Agreement Property"
     _inherit = 'agreement'
